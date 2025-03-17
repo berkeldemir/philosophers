ac5437ef2a65ac4e03db07bf7705045a4b1fb3f4
Berk Muzaffer Eldemir <beldemir@k1m32s04.42istanbul.com.tr>
2025-03-17 20:48:49 +0300

    it is not working at all now, but header file basically done.

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 09:38:14 by beldemir          #+#    #+#             */
-/*   Updated: 2025/03/11 19:38:51 by beldemir         ###   ########.fr       */
+/*   Updated: 2025/03/17 20:47:32 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,48 @@
 
 void	*routine(void *arg)
 {
-	printf("\nphilo_id:%i\n", *(int *)arg);
+	//printf("\nphilo_id:%i\n", *(int *)arg);
 	return (NULL);
 }
 
-static long	get_time()
+uint64_t	get_time(void)
 {
-	struct timeval	tv;
-
+	struct timeval tv;
 	gettimeofday(&tv, NULL);
-	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	return ((tv.tv_sec * 1000ULL) + (tv.tv_usec / 1000ULL));
 }
 
-static void	print_time(long init_time)
+void	elapsed_time(uint64_t init_time)
 {
-	printf("%ld", get_time() - init_time);
+	printf("%s%05lu: %s", PURPLE, (get_time() - init_time), DEFAULT);
 }
 
-int	main(int ac, char **av)
+int	check_args(int ac, char **av, t_app *app)
+{
+	if (ac == 5 || ac == 6)
+	{
+		while (--ac > 0)
+			if (!ft_isnum(av[ac]))
+				return (-1);
+		return (0);
+	}
+	else
+		return (-1);
+}
+
+int main(int ac, char **av)
 {
 	t_app		app;
-	long init_time = get_time();
-	if (init(&app) == -1)
+
+	if (check_args(ac, av, &app) == -1)
 		return (1);
-	print_time(init_time);
-	usleep(3000);
-	printf("\n");
-	print_time(init_time);
-	//printf("sec : %ld\n", app.tv.tv_sec);
-	//printf("msec: %ld\n", app.tv.tv_usec);
+	init(&app);
+	app.init_time = get_time();
+	elapsed_time(app.init_time);
+	sleeping(1);
+	usleep(2000000); // 2 saniye bekle
+	sleep(2);
+	elapsed_time(app.init_time);
+	died(4);
+	return 0;
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 09:39:54 by beldemir          #+#    #+#             */
-/*   Updated: 2025/03/11 19:34:15 by beldemir         ###   ########.fr       */
+/*   Updated: 2025/03/17 20:46:13 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 # include <stdio.h>
 # include <sys/time.h>
 # include <unistd.h>
+# include <stdint.h>
+# include <string.h>
 
 # define DEFAULT "\033[0m"
 # define RED "\033[38;2;220;80;80m"      // Soft Red
@@ -24,26 +26,54 @@
 # define BLUE "\033[38;2;100;150;255m"   // Soft Blue
 # define ORANGE "\033[38;2;255;170;80m"  // Soft Orange
 # define YELLOW "\033[38;2;255;225;120m" // Soft Yellow
+# define PURPLE "\033[38;2;180;130;255m"  // Soft Purple
 
+typedef enum e_bool
+{
+	TRUE = 1,
+	FALSE = 0
+}	t_bool;
+
+typedef	struct s_fork
+{
+	pthread_mutex_t	fork;
+	int				fork_id;
+}	t_fork;
+
+typedef struct s_philo
+{
+	pthread_t	thread_id;
+	int			philo_id;
+	int			meals_had;
+	t_bool		full;
+	uint64_t	last_eaten;
+	t_fork		*l_fork;
+	t_fork		*r_fork;
+}	t_philo;
 
 typedef struct s_app
 {
-	struct timeval	tv;
-	pthread_t		*philo;
-	int				*philo_id;
-	int				count;
-	//int	philo_count;
-}   t_app;
+	t_fork		*forks;
+	t_philo		*philos;
+	int			philo_count;
+	uint64_t	time_to_die;
+	uint64_t	time_to_eat;
+	uint64_t	time_to_slp;
+	uint64_t	init_time;
+	t_bool		quit;
+}	t_app;
 
 int		init(t_app *app);
 void	*routine(void *arg);
 
-
-// ACTIONS
+// Actions
 void	taken_a_fork(int philo_id);
 void	eating(int philo_id);
 void	sleeping(int philo_id);
 void	thinking(int philo_id);
 void	died(int philo_id);
+
+// Utils
+int		ft_isnum(char *str);
 
 #endif
