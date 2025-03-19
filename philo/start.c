9643b2c8b716af43e0726f71ffe7a6d02219feb3
berkeldemir@kali <berkeldemir@outlook.com>
2025-03-19 16:11:24 -0400

    minor changes made.

--- philo/start.c
+++ philo/start.c
@@ -6,7 +6,7 @@
 /*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 12:46:40 by beldemir          #+#    #+#             */
-/*   Updated: 2025/03/18 21:16:27 by beldemir         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:18:38 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@ static void	*routine(void *arg)
 	t_app		*app;
 
 	app = (t_app *)arg;
+	if (!app)
+		return (NULL);
 	return (NULL);
 }
 
-int	start(t_app *app)
-{
+int	start_philos(t_app *app)
+{	
 	int	i;
 
 	app->philos = (t_philo *)malloc(sizeof(t_philo) * app->philo_count);
 	if (!app->philos)
 		return (1);
+	app->philos[0].l_fork = &app->forks[app->philo_count - 1];
+	app->philos[0].r_fork = &app->forks[0];
 	i = -1;
 	while (++i < app->philo_count)
 	{
@@ -34,12 +38,33 @@ int	start(t_app *app)
 		app->philos[i].meals_had = 0;
 		app->philos[i].full = FALSE;
 		app->philos[i].last_eaten = 0;
-		app->philos[i].l_fork = NULL;
-		app->philos[i].r_fork = NULL;
+		if (i != 0)
+		{
+			app->philos[i].l_fork = &app->forks[i - 1];
+			app->philos[i].r_fork = &app->forks[i];
+		}
 		pthread_create(&app->philos[i].thread, NULL, routine, (void *)&app);
 	}
 	i = -1;
 	while (++i < app->philo_count)
 		pthread_join(app->philos[i].thread, NULL);
+	return (0);
+}
+
+int	start(t_app *app)
+{
+	int	i;
+
+	app->forks = (t_fork *)malloc(sizeof(t_fork) * app->philo_count);
+	if (!app->forks)
+		return (1);
+	i = -1;
+	while (++i < app->philo_count)
+	{
+		pthread_mutex_init(&app->forks[i].fork, NULL);
+		app->forks[i].fork_id = i + 1;
+	}
+	if (start_philos(app) != 0)
+		return (1);
 	return (0);
 }
