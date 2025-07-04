f9fdcf982c5b63af694dab549eaad5e218231c7f
beldemir <beldemir@student.42istanbul.com.tr>
2025-07-04 15:10:55 +0300

    philo_final basic structure builded.

--- philo_final/philo_bonus.c
+++ philo_final/philo_bonus.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 15:01:31 by beldemir          #+#    #+#             */
+/*   Updated: 2025/07/04 15:01:32 by beldemir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./philo_bonus.h"
 
 static int	arg_check(int ac, char **av)
@@ -19,7 +31,15 @@ static int	table_init_semaphores(t_table *table)
 	sem_unlink("/s_forks");
 	sem_unlink("/s_print");
 	sem_unlink("/s_read");
-	ütable->s_forks = sem_open("/s_forks", O_CREAT, 0644, num_philos);
+	table->s_start = sem_open("s_start", O_CREAT | O_EXCL, 0666, 0);
+	table->s_forks = sem_open("/s_forks", O_CREAT | O_EXCL, 0666, \
+	table->philo_count);
+	table->s_print = sem_open("/s_print", O_CREAT | O_EXCL, 0666, 1);
+	table->s_read = sem_open("/s_read", O_CREAT | O_EXCL, 0666, 1);
+	if (table->s_start == SEM_FAILED || table->s_forks == SEM_FAILED\
+	 || table->s_print == SEM_FAILED || table->s_read == SEM_FAILED)
+		return (-1);
+	return (0);
 }
 
 static int	table_init(int ac, char **av, t_table *table)
@@ -45,22 +65,43 @@ static int	philos_init(t_table *table)
 	int	i;
 
 	table->pids = (pid_t *)malloc(sizeof(pid_t) * table->philo_count);
-	if (!table->pids)
+	table->phis = (t_phi *)malloc(sizeof(t_phi) * table->philo_count);
+	if (!table->pids || !table->phis)
 		return (-1);
 	i = -1;
-	while (++i < )
+	while (++i < table->philo_count)
+	{
+		table->pids[i] = fork();
+		if (table->pids[i] == 0)
+		{
+			table->phis[i].id = i + 1;
+			table->phis[i].ate = 0;
+			table->phis[i].last_meal = 0;
+			table->phis[i].table = table;
+			if (!pthread_create(&table->phis[i].self, NULL, ph_routine, \
+			&table->phis[i]));
+				return (-1);
+			routine(table->phis[i]);
+			return (i + 1);
+		}
+	}
+	return (0);
 }
 
 int main(int ac, char **av)
 {
 	t_table table;
+	int		philos_init_ret_val;
 
 	if (arg_check(ac, av) != 0)
 		return (1);
 	if (table_init(ac, av, &table) != 0)
-		return (2);
-	if (philos_init(&table) != 0)
-		return (3);
-
+		return (exit_noleak(&table), 2);
+	philos_init_ret_val = philos_init(&table);
+	if (philos_init_ret_val == -1)
+		return (exit_noleak(&table), 3);
+	if (philos_init_ret_val > 0)
+		return (exit_noleak(&table), 0);
+	dining(&table);
 	return (0);
 }
